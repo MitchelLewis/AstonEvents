844f5283144fa9923dd349351235418c5d40aec6
Lewis Mitchell <52708269+MitchelLewis@users.noreply.github.com>
2021-06-03 19:19:00 +0100

    Added auth and event creation

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\EventsController;
-use App\Http\Controllers\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
