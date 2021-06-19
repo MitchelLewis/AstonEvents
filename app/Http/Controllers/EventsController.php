977deb092a60af6bae64f766dafb0f7faa7c32fc
Lewis Mitchell <52708269+MitchelLewis@users.noreply.github.com>
2021-06-19 17:51:59 +0100

    Fixed where users could show interest numerous times + removed forgot password link + organiser on event page

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use App\Models\Image;
+use App\Models\Organiser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -20,27 +21,31 @@ class EventsController extends Controller
             $relatedEvent = Event::findOrFail($relatedContentId);
         }
         $eventImg = Image::select('filename')->where('event_id', $event->id)->get();
+        $eventOrganiser = Organiser::select('name')->where('id', $event->eventOrganiserId)->get()->first();
         if($eventImg->isEmpty()) {
             $eventImg = "event_placeholder.jpg";
         } else {
             $firstImg = $eventImg->first();
             $eventImg = $firstImg->filename;
         }
-        return view('eventPage', ['event' => $event, 'relatedEvent' => $relatedEvent, 'eventImg' => $eventImg]);
+        return view('eventPage', ['event' => $event, 'relatedEvent' => $relatedEvent, 'eventImg' => $eventImg, 'organiser' => $eventOrganiser]);
     }
 
     public function onSubmit(Request $request, String $id) {
         $answer = $request -> interested_btn;
         $model = Event::findOrFail(htmlspecialchars($id));
-        if($answer == "Interested") {
+        if($answer == "Interested" && !in_array($id, $request->session()->get('eventInterest', []))) {
             DB::table('events')
             ->where('id', htmlspecialchars($id))
             ->increment('interestRanking', 1);
-        } else if($answer == "Not interested" && ($model -> interestRanking - 1 >= 0)) {
+            $request->session()->push('eventInterest', $id);
+        } else if($answer == "Not interested" && ($model -> interestRanking - 1 >= 0) && !in_array($id, $request->session()->get('eventInterest', []))) {
             DB::table('events')
             ->where('id', htmlspecialchars($id))
             ->increment('interestRanking', -1);
+            $request->session()->push('eventInterest', $id);
         }
-        return view('eventPage', Event::findOrFail(htmlspecialchars($id)));
+        
+        return $this->onPageLoad($request, $id);
     }
 }
