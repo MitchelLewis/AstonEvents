777f5b00c90747fb3fb2c85f655bb42b8693267d
Lewis Mitchell <52708269+MitchelLewis@users.noreply.github.com>
2021-06-22 18:33:18 +0100

    Removed auth needed on email page

--- app/Mail/EmailSender.php
+++ app/Mail/EmailSender.php
@@ -23,11 +23,11 @@ class EmailSender extends Mailable
         //
     }
 
-    public function sendMail(String $name, String $recipient, String $msg) {
+    public function sendMail(String $recipient, String $msg) {
         $mj = Mailjet::getClient();
         $body = [
             'FromEmail' => "180212228@aston.ac.uk",
-            'FromName' => $name,
+            'FromName' => "Aston Events",
             'Subject' => "You have a new email from AstonEvents",
             'Text-part' => $msg,
             'Recipients' => [['Email' => $recipient]]];
